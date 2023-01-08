3186753a2d5abd490e3b4d2b827c26bac1cac847
HaniaFlores <hanniaflores03@gmail.com>
2023-01-07 20:50:15 -0600

    Finished Home Portal

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 // Select the HTML element to manipulate
 update = new Date(document.lastModified)
+const d = new Date();
 theTime = update.toLocaleString()
 
 //Get the date and print it.
 const fulldate = `${theTime}`;
 document.querySelector("#date").textContent = fulldate;
+
+const currentYear = d.getFullYear();
+theYear = `${currentYear}`;
+document.querySelector("#currentYear").textContent = theYear;
