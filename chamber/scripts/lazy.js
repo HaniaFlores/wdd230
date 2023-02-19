5191063fd208847f2c938ebdad248f28b1d2a7f7
HaniaFlores <hanniaflores03@gmail.com>
2023-02-18 21:20:11 -0600

    Discover Page

--- chamber/scripts/lazy.js
+++ chamber/scripts/lazy.js
@@ -25,3 +25,16 @@
   document.querySelectorAll('picture').forEach((picture) => {
     observer.observe(picture);
   });
+
+// Get the last visit date from local storage
+		var lastVisit = localStorage.getItem("discoverPageLastVisit");
+		if (lastVisit) {
+			// Calculate the number of days since the last visit
+			var daysSinceLastVisit = Math.round((new Date() - new Date(lastVisit)) / (1000 * 60 * 60 * 24));
+			// Display the number of days on the page
+			var message = "Welcome back! It's been " + daysSinceLastVisit + " days since your last visit.";
+			// Replace "message-element" with the ID of the element where you want to display the message
+			document.getElementById("message-element").textContent = message;
+		}
+		// Save the current visit date to local storage
+		localStorage.setItem("discoverPageLastVisit", new Date());
