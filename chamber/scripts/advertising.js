9b30b30f3cef252c12415f82bf249803c6429551
HaniaFlores <hanniaflores03@gmail.com>
2023-03-18 22:23:35 -0600

    Spotlight

--- chamber/scripts/advertising.js
+++ chamber/scripts/advertising.js
@@ -1,34 +1,55 @@
 async function getBusinessData() {
-  const response = await fetch("json/data.json");
-  const data = await response.json();
-  const silverAndGoldCompanies = data.businesses.filter(business => {
-    return business.membershipLevel === "Silver" || business.membershipLevel === "Gold";
-  });
+    const response = await fetch("json/data.json");
+    const data = await response.json();
     
-  const selectedCompanies = [];
-  while (selectedCompanies.length < 3) {
-    const randomIndex = Math.floor(Math.random(0, silverAndGoldCompanies.length));
-    const selectedCompany = silverAndGoldCompanies[randomIndex];
-    if (!selectedCompanies.includes(selectedCompany)) {
-      selectedCompanies.push(selectedCompany);
-    }
-  }
-  const company1Name = document.getElementById('company1-name');
-  const company1Description = document.getElementById('company1-description');
-  const company2Name = document.getElementById('company2-name');
-  const company2Description = document.getElementById('company2-description');
-  const company3Name = document.getElementById('company3-name');
-  const company3Description = document.getElementById('company3-description');
-  if (selectedCompanies.length >= 2) {
-    company1Name.textContent = selectedCompanies[0].name;
-    company1Description.textContent = selectedCompanies[0].description;
-    company2Name.textContent = selectedCompanies[1].name;
-    company2Description.textContent = selectedCompanies[1].description;
-  }
-  if (selectedCompanies.length === 3) {
-    company3Name.textContent = selectedCompanies[2].name;
-    company3Description.textContent = selectedCompanies[2].description;
-  }
+    displayBusinesses(data.businesses);
+}
+
+const displayBusinesses = function (businesses) {
+    const container = document.querySelector("#spot-container");
+    
+    const list = businesses.filter((business) => {
+        return business.membershipLevel === "Gold" || business.membershipLevel === "Silver"
+    });
+
+    const randomBusiness = list.sort(() => Math.random() - 0.5)
+    
+    const selectedBusinesses = randomBusiness.slice(0, 3);
+
+    selectedBusinesses.forEach((business, index) => {
+
+      let section = document.createElement("section");
+      section.setAttribute("id", `spot${index + 1}`);
+      let logo = document.createElement("img");
+      let name = document.createElement("h2");
+      let address = document.createElement("p");
+      let phoneNumber = document.createElement("p");
+      let website = document.createElement("a");
+
+      // Build the logo
+      logo.setAttribute("src", business.logo);
+      logo.setAttribute("alt", `${business.name} logo.`);
+      logo.setAttribute("loading", "lazy");
+
+      // Build the name, address, phone number, and website
+      name.textContent = business.name;
+      address.textContent = business.address;
+      phoneNumber.textContent = business.phoneNumber;
+      website.textContent = "Website";
+
+      website.setAttribute("href", business.website);
+      website.setAttribute("target", "_blank");
+
+      // Append the elements
+      section.appendChild(logo);
+      section.appendChild(name);
+      section.appendChild(address);
+      section.appendChild(phoneNumber);
+      section.appendChild(website);
+
+      container.appendChild(section);       
+    });
+
 }
 
 getBusinessData();
