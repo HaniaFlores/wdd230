b0887c7f636127a13c28a34408f612b81fde12cd
HaniaFlores <hanniaflores03@gmail.com>
2023-04-05 17:15:52 -0600

    .

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('#drink-form');
+const output = document.querySelector('#output');
+const submit = document.querySelector("#submitBtn");
 
 // populate the fruit select inputs with options from the JSON data source
 fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
@@ -7,8 +9,6 @@ fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
     const options = fruits.map(fruit => `<option value="${fruit.id}">${fruit.name}</option>`);
     const selectInputs = document.querySelectorAll('select');
       selectInputs.forEach((selectInput) => {
-          /* selectInput.style.fontSize = "30px"; */
           selectInput.innerHTML = options.join('');
       });
   });
-
